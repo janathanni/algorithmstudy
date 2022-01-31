7a82edc7f3a79381bccd1343f4ef75ddc7e821cb
telemain1@hotmail.com <telemain1@hotmail.com>
2022-01-31 22:07:12 +0900

    okay I'm confused now.

--- inflearn_algorithmstudy/뒤집은소수.py
+++ inflearn_algorithmstudy/뒤집은소수.py
@@ -1,8 +1,31 @@
-def reverse(x):
-    pass
+def reverse(n, x):
+    for i in range(n):
+        x[i] == int(x[i][::-1])
+    
+    return isPrime(x)
 
 def isPrime(x):
-    pass
+    num_lists = [0 for _ in range(0, max(x)+3)]
+
+    for i in range (2, int(max(x)+1)):
+
+        if num_lists[i] == 1:
+            continue
+        
+        else:
+            for j in range (i+1, int(max(x)+1)):
+                num_lists[j]==1
+
+    prime_lists = []
+
+    for number in x:
+        if number in num_lists:
+            prime_lists.append(str(number))
+
+    return (' '.join(prime_lists))
+
 
 n = int(input())
-nums = list(map(int, input().split()))
+nums = input().split()
+
+reverse(n, nums)
